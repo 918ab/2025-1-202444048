148d3e26ee6f8ba0fd398e8150a3e61b11efde5d
918 <107310740+918ab@users.noreply.github.com>
2025-04-02 19:47:19 +0900

    Add files via upload

---
+++ C#프로그래밍/Week03Homework/Week03Homework/Week03Homework/FromMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Week03Homework
+{
+    public partial class FormMain : Form
+    {
+        private double beforeNumber = 0;
+        private string label = null;
+        private double inputNumber = 0;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        private void btnNumer_Click(object sender, EventArgs e)
+        {
+            Button target = (Button) sender;
+            double number = double.Parse(inputNumber + target.Text);
+            lblNumbers.Text = number.ToString();
+            inputNumber = number;
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button target = (Button)sender;
+            double afterNumber = 0;
+            if (beforeNumber == 0){
+                beforeNumber = inputNumber;
+            }
+            switch (label)
+            {
+                case "÷":
+                    afterNumber = beforeNumber / inputNumber;
+                    break;
+                case "X":
+                    afterNumber = beforeNumber * inputNumber;
+                    break;
+                case "-":
+                    afterNumber = beforeNumber - inputNumber;
+                    break;
+                case "+":
+                    afterNumber = beforeNumber + inputNumber;
+                    break;
+                default:
+                    afterNumber = inputNumber;
+                    break;
+            }
+            lblExpression.Text = afterNumber + " " + target.Text + " ";
+            if (target.Text == "=") {
+                if (beforeNumber != 0 && inputNumber != 0){
+                    lblExpression.Text = beforeNumber + " " + label + " " + inputNumber + " = " + afterNumber;
+                    label = null;
+                    inputNumber = afterNumber;
+                }
+                else{
+                    MessageBox.Show("숫자를 입력해주세요");
+                    return;
+                }
+            }else{
+                inputNumber = 0;
+                label = target.Text;
+            }
+            lblNumbers.Text = afterNumber.ToString();
+            beforeNumber = afterNumber;
+            //MessageBox.Show(label +"/"+ beforeNumber + "/" + inputNumber);
+        }
+    }
+}
---
+++ C#프로그래밍/Week03Homework/Week04Homework/Week04Homework/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04Homework
+{
+    //Object 클래스를 상속한 Department
+    class Department : Object
+    {
+        //인스턴스 필드(변수), 멤버 변수
+        public string Code;
+        public string Name;
+
+        //python의 __str__()와 동일
+        public override string ToString()
+        {
+            return $"[{Code}] {Name}";
+        }
+    }
+}
