dd974adaa0b0bc43987fc1eb27ad47fa67369b82
918 <107310740+918ab@users.noreply.github.com>
2025-05-14 19:44:20 +0900

    Add files via upload

---
+++ C#프로그래밍/ZooApp/ZooApp/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp
+{
+    abstract class Animal
+    {
+        private string _name;
+        protected int _level;
+        protected COLOR _color;
+        
+        public string Name { get { return _name; }  }
+        public COLOR Color { get { return _color; } }
+        public int Level { get { return _level; } set { _level = value;  } }
+        
+        //이름,색깔,나이
+        public Animal(string name, COLOR color)
+        {
+            _name = name;
+            _color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"ANIMAL:{_name}";
+        }
+        public virtual void Eat()
+        {
+            AddLevel(3);
+        }
+        public void Play()
+        {
+            AddLevel(2);
+        }
+        abstract protected bool AddLevel(int level);
+    }
+}
---
+++ C#프로그래밍/ZooApp/ZooApp/RobotDog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp
+{
+    class RobotDog : Dog, IRobot
+    {
+        public RobotDog(string name, COLOR color, int year)
+            : base(name,color,year)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"ROBOTDOG:{Name}";
+        }
+
+        //자동구현 프로퍼티
+        public int BatteryLevel { get; set; }
+        
+        public void Charge()
+        {
+            BatteryLevel = 1000;
+        }
+    }
+}
